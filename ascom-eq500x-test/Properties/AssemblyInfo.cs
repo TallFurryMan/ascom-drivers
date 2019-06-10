6234b50db6e7577b784a8cb93a23961c5a55f20a
TallFurryMan <eric.dejouhanet@gmail.com>
2019-06-10 23:37:14 +0200

    Initial edit, created from ASCOM driver template

---
+++ ascom-eq500x-test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ascom-eq500x-test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ascom-eq500x-test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("097cfa68-d08d-4c93-9bf9-8f0fe051b599")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ascom-eq500x-test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ascom_eq500x_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var device = new ASCOM.DriverAccess.Telescope("ASCOM.EQ500X.Telescope"))
+            {
+                Assert.AreEqual("Omegon EQ500X", device.Name);
+                Assert.AreEqual("ASCOM Telescope Driver for EQ500X.", device.Description);
+                Assert.AreEqual("6.4", device.DriverVersion);
+            }
+        }
+    }
+}
